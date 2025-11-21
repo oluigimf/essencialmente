79f375cc9f16e876c11e0bc654a11905be6cff8a
Eduardo Metzdorf <metzdorf.eduardo@hotmail.com>
2025-11-21 11:20:37 -0300

    avaliar gpt

--- essencialmente/imc/models.py
+++ essencialmente/imc/models.py
@@ -4,3 +4,4 @@ class Avaliar_IMC(models.Model):
     CPF = models.CharField(max_length=11, primary_key=True, unique=True, null=False, blank=False)
     Nome = models.CharField(max_length=150, null=False, blank=False)
     
+    
