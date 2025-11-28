9d783c081fb516bc9902082917ac199eed3a3e31
Eduardo Metzdorf <metzdorf.eduardo@hotmail.com>
2025-11-28 11:02:32 -0300

    tela de avaliar imc pronta, mas não calcula

--- essencialmente/imc/forms.py
+++ essencialmente/imc/forms.py
@@ -5,3 +5,13 @@ class AvaliarIMCForm(forms.ModelForm):
     class Meta:
         model = AvaliarIMC
         fields = ["cpf", "nome", "sexo", "data_nascimento", "escola", "peso", "altura", "foto"]
+        widgets = {
+            "cpf": forms.TextInput(attrs={"class": "input-field"}),
+            "nome": forms.TextInput(attrs={"class": "input-field"}),
+            "sexo": forms.Select(attrs={"class": "input-field"}),
+            "data_nascimento": forms.DateInput(attrs={"class": "input-field", "type": "date"}),
+            "escola": forms.TextInput(attrs={"class": "input-field"}),
+            "peso": forms.NumberInput(attrs={"class": "input-field"}),
+            "altura": forms.NumberInput(attrs={"class": "input-field"}),
+            "foto": forms.FileInput(attrs={"class": "input-field"}),
+        }
