923002faecbe873bc05a08c11ed6e938fdb0cb31
Luigi Mezzalira Franzosi <luigi.mf2007@aluno.ifsc.edu.br>
2025-12-02 00:27:01 -0300

    Index Corrigido

--- essencialmente/essencialmente/settings.py
+++ essencialmente/essencialmente/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'essencialmente',
         'USER': 'postgres',
-        'PASSWORD': 'metzdorf',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432'
     }
